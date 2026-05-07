a3406229bec67df05612f7cc0a8559e4a685acb0
svijaysh <svijaysh@nrel.gov>
2026-05-07 13:57:38 -0600

    Change round to ceil when computing n_days

--- h2integrate/control/control_strategies/storage/plm_optimized_storage_controller.py
+++ h2integrate/control/control_strategies/storage/plm_optimized_storage_controller.py
@@ -303,7 +303,7 @@ class PeakLoadManagementOptimizedStorageController(PyomoStorageControllerBaseCla
                 # Solve the optimzation problem
                 self.solve_dispatch_model(
                     start_time=window_start,
-                    n_days=int(round(self.n_timesteps * self.dt_seconds / 86400)),
+                    n_days=int(math.ceil(self.n_timesteps * self.dt_seconds / 86400)),
                 )
 
                 # Count new discharge events to track the monthly cap.
